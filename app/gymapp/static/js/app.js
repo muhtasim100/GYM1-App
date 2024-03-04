c7e35f9707bc33f793c8c49eb549e94f81760541
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2024-03-04 02:49:29 +0000

    feat: service worker script created and registered
    feat: service worker script created and registered in sw.js and app.js.

---
+++ app/gymapp/static/js/app.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/static/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    });
+  }
+  
---
+++ app/sw.js
@@ -0,0 +1,30 @@
+// sw.js
+const CACHE_NAME = 'gymapp-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/styles.css',
+  '/static/js/app.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
