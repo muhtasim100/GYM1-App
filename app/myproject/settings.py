69c6b2ae6f7dcbb732a02b69eca7023cbf8eb785
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2024-03-13 23:29:33 +0000

    feat: start of database.
    feat: start of database by modifying models.

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gymapp',
+    'gymapp.apps.GymappConfig',
 ]
 
 MIDDLEWARE = [
