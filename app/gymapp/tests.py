6362eba2ca1e343bed9b4f07f8d1e18d258b3b0c
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2024-03-14 02:05:06 +0000

    feat: TDD unit test for Models.
    feat: TDD unit test to check if item and list creation works.

--- app/gymapp/tests.py
+++ app/gymapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from gymapp.models import ToDoList, Item
 
 # Create your tests here.
+
+class ModelsTest(TestCase):
+    def test_list(self):
+        todolist = ToDoList(name="Test_List")
+        todolist.save()
+        item = Item(todolist=todolist, text="Item1", complete=False)
+        item.save()
+        print(ToDoList.objects.all())
+
+
+
+        
