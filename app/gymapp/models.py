13a99dbfa9c4499bca45cf78fe7893efb659b172
zjac350 <md.mahmood.2020@live.rhul.ac.uk>
2024-03-15 01:28:35 +0000

    feat: working on models.
    feat: working on models.

--- app/gymapp/models.py
+++ app/gymapp/models.py
@@ -1,18 +1,11 @@
 from django.db import models
-
-# Create your models here.
-# Code from Tech with Tim on Youtube.
-class ToDoList(models.Model):
-	name = models.CharField(max_length=200)
-
-	def __str__(self):
-		return self.name
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import qrcode
+from io import BytesIO
+from django.core.files import File
 
 
-class Item(models.Model):
-	todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-	text = models.CharField(max_length=300)
-	complete = models.BooleanField()
-
-	def __str__(self):
-		return self.text
+class CustomUser(AbstractUser):
+    pass
